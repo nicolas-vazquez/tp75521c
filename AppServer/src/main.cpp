aa87899515c0a11aafe88eed5f28d0d472319ee0
fjfarina <74123698Fede>
2016-03-24 20:19:04 -0300

    Added index page and mock login ws

--- AppServer/src/main.cpp
+++ AppServer/src/main.cpp
@@ -3,22 +3,14 @@
 #include "mongoose/mongoose.h"
 
 static const char *html_form =
-  "<html><body>POST example."
-  "<form method=\"POST\" action=\"/handle_post_request\">"
-  "Input 1: <input type=\"text\" name=\"input_1\" /> <br/>"
-  "Input 2: <input type=\"text\" name=\"input_2\" /> <br/>"
-  "<input type=\"submit\" />"
-  "</form></body></html>";
+  "<html><body><h1>AppServer</h1></body></html>";
 
 static int handler(struct mg_connection *conn) {
   char var1[500], var2[500];
 
-  if (strcmp(conn->uri, "/handle_post_request") == 0) {
-    // User has submitted a form, show submitted data and a variable value
-    // Parse form data. var1 and var2 are guaranteed to be NUL-terminated
-    mg_get_var(conn, "input_1", var1, sizeof(var1));
-    mg_get_var(conn, "input_2", var2, sizeof(var2));
-
+  if (strcmp(conn->uri, "/login") == 0) {
+    mg_get_var(conn, "username", var1, sizeof(var1));
+    mg_get_var(conn, "password", var2, sizeof(var2));
     // Send reply to the client, showing submitted form values.
     // POST data is in conn->content, data length is in conn->content_len
     mg_send_header(conn, "Content-Type", "text/plain");
@@ -38,10 +30,14 @@ static int handler(struct mg_connection *conn) {
 }
 
 int main(void) {
+
+    // Server configuration
   struct mg_server *server = mg_create_server(NULL);
   mg_set_option(server, "listening_port", "8080");
   mg_add_uri_handler(server, "/", handler);
   printf("Starting on port %s\n", mg_get_option(server, "listening_port"));
+
+  //Start server
   for (;;) {
     mg_poll_server(server, 1000);
   }
