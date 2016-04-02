d9e0ee629095f3572c8ff5e2ac4cf02013e2f924
Nicolás Vazquez <nicolas.vazquez@fdvsolutions.com>
2016-04-02 02:47:45 -0300

    Rocksdb library working
    Former-commit-id: 5feb4c49e5e16cfe7f7a600c6c0260955da158ec

--- AppServer/src/controllers/BaseController.h
+++ AppServer/src/controllers/BaseController.h
@@ -8,11 +8,15 @@
 #include <mongoose/JsonController.h>
 #include <mongoose/Server.h>
 #include "../errors/Error.h"
+#include "rocksdb/db.h"
 
 using namespace std;
 using namespace Mongoose;
+using namespace rocksdb;
 
 class BaseController : public JsonController {
+private:
+    DB* db;
 
 protected:
     void sendError(JsonResponse &response, vector<Error *> &errors, int responseCode);
