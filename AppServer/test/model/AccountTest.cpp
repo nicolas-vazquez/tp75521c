9fc378a29a3407fef8e60ad3b737248079d8b47d
fjfarina <74123698Fede>
2016-05-09 23:13:09 -0300

    Account test fix

--- AppServer/test/model/AccountTest.cpp
+++ AppServer/test/model/AccountTest.cpp
@@ -13,54 +13,52 @@ AccountTest::AccountTest() {
 
 
 void AccountTest::setUp() {
-/*    testAccount.setUsername("anUsername1");
-    testAccount.setPassword("aPassword");
+    testAccount.setUsername("username");
+    testAccount.setPassword("password");
     testAccount.addKeepAccount("1");
     testAccount.addKeepAccount("2");
     testAccount.addKeepAccount("3");
     testAccount.addTossAccount("4");
     testAccount.addTossAccount("5");
-    testAccount.save();*/
+    testAccount.save();
 }
 
 
 void AccountTest::toJSON() {
 
-    /*const Value &value = testAccount.toJSON();
+    const Value &value = testAccount.toJSON();
 
     //Tests that all fields are serialized in json result
     CPPUNIT_ASSERT(!value.get("username", "no empty").empty());
     CPPUNIT_ASSERT(!value.get("password", "no empty").empty());
     CPPUNIT_ASSERT(!value.get("keptAccounts", "no empty").empty());
-    CPPUNIT_ASSERT(!value.get("tossedAccounts", "no empty").empty());*/
-    CPPUNIT_ASSERT(true);
+    CPPUNIT_ASSERT(!value.get("tossedAccounts", "no empty").empty());
 }
 
 void AccountTest::fromJSON() {
-    /* Account account;
-     account.setUsername("anUsername1");
-     account.fetch();
-     CPPUNIT_ASSERT(account.getUsername() == "anUsername1");
-     CPPUNIT_ASSERT(account.getPassword() == "aPassword");
+    Account account;
+    account.setUsername("username");
+    account.fetch();
+    CPPUNIT_ASSERT(account.getUsername() == "username");
+    CPPUNIT_ASSERT(account.getPassword() == "password");
 
-     const vector<string> &keptAccounts = account.getKeptAccounts();
+    const vector<string> &keptAccounts = account.getKeptAccounts();
 
-     CPPUNIT_ASSERT(keptAccounts.size() == 3);
-     CPPUNIT_ASSERT(keptAccounts.at(0) == "1");
-     CPPUNIT_ASSERT(keptAccounts.at(1) == "2");
-     CPPUNIT_ASSERT(keptAccounts.at(2) == "3");
+    CPPUNIT_ASSERT(keptAccounts.size() == 3);
+    CPPUNIT_ASSERT(keptAccounts.at(0) == "1");
+    CPPUNIT_ASSERT(keptAccounts.at(1) == "2");
+    CPPUNIT_ASSERT(keptAccounts.at(2) == "3");
 
 
-     const vector<string> &tossedAccounts = account.getTossedAccounts();
-     CPPUNIT_ASSERT(tossedAccounts.size() == 2);
-     CPPUNIT_ASSERT(tossedAccounts.at(0) == "4");
-     CPPUNIT_ASSERT(tossedAccounts.at(1) == "5");*/
-    CPPUNIT_ASSERT(true);
+    const vector<string> &tossedAccounts = account.getTossedAccounts();
+    CPPUNIT_ASSERT(tossedAccounts.size() == 2);
+    CPPUNIT_ASSERT(tossedAccounts.at(0) == "4");
+    CPPUNIT_ASSERT(tossedAccounts.at(1) == "5");
 }
 
 
 void AccountTest::tearDown() {
-    //testAccount.remove();
+    testAccount.remove();
 }
 
 
