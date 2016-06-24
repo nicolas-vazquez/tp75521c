3f70d1b4ddf37ff2ef45dc360d722be106865a85
Gisela Daye <gisedaye@gmail.com>
2016-06-24 02:04:07 -0300

    coverage 71.2%

--- AppServer/test/model/ChatTest.cpp
+++ AppServer/test/model/ChatTest.cpp
@@ -8,22 +8,32 @@
 CPPUNIT_TEST_SUITE_REGISTRATION(ChatTest);
 
 void ChatTest::toJSON() {
-    Chat chat("Gise");
-    chat.setUser("pepe");
-    const Value &value = chat.toJSON();
+    Chat testChat2("gii");    
+    testChat2.setUser("nico");
+    Value value2;
+    value2["message"] = "Hola gii";
+    testChat2.update(value2);
+    testChat2.save();
+    const Value &value = testChat2.toJSON();
     CPPUNIT_ASSERT(!value.get("sender", "no empty").empty());
 }
 
 void ChatTest::fromJSON() {
-    Chat chat("Gise");
-    chat.setUser("pepe");
-    //chat.update("Hola Gise");
-    chat.fetch();
-    //const vector<string> &messages = chat.getMessages();
-
-    CPPUNIT_ASSERT(true);
+    Chat testChat("nico", "fede");    
+    Json::Value value;
+    value["message"] = "Holaa";
+    testChat.update(value);
+    testChat.save();
+    testChat.fetch();    
+    const Json::Value &messages = testChat.getMessages();
+    CPPUNIT_ASSERT(messages[0].get("message", "") == "Holaa");
 }
 
+void ChatTest::validateIds() {
+    Chat testChat("nico", "fede");  
+    Chat testChat3("fede", "nico");    
+    CPPUNIT_ASSERT(testChat.getId() == testChat3.getId());
+}
 
 ChatTest::~ChatTest() {
 
--- AppServer/test/model/ChatTest.h
+++ AppServer/test/model/ChatTest.h
@@ -15,6 +15,7 @@ class ChatTest : public TestFixture {
 CPPUNIT_TEST_SUITE(ChatTest);
         CPPUNIT_TEST(toJSON);
         CPPUNIT_TEST(fromJSON);
+        CPPUNIT_TEST(validateIds);
     CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -22,6 +23,8 @@ public:
     void toJSON();
 
     void fromJSON();
+
+    void validateIds();
 
     virtual ~ChatTest();
 };
