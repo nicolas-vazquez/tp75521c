ab8fde27b3a62dc250a7ad9e8e2f199b79fe525d
fjfarina <74123698Fede>
2016-06-25 07:09:55 -0300

    Functional test fixes on AccountTestCases

--- AppServer/functionalTests/AccountTestCases.py
+++ AppServer/functionalTests/AccountTestCases.py
@@ -8,7 +8,7 @@ class AccountTest(unittest.TestCase):
 	def __init__(self, *args, **kwargs):
 		super(AccountTest, self).__init__(*args, **kwargs)
 		self.__api_base_url = "http://localhost:8083/"
-		self._url_usuario = "/api/accounts/"
+		self._url_usuario = "api/accounts/"
 
 	@classmethod
 	def setUpClass(cls):
@@ -16,9 +16,9 @@ class AccountTest(unittest.TestCase):
 		cls._username = cls._username_long[0:10]		
 		cls._usernamelike = cls._username + "like"
 
-	#Successful signup	
+	#Successful signup
 	def test_1(self):
-		payload = {'username': "'"+ self._username +"'",'password':'pass'}
+		payload= {"'username': '"+ self._username +"', 'password': 'pass', 'name': 'name', 'age': 20, 'gender':'M', 'email':'email@email.com', 'latitude': '-34.58', 'longitude': '-58.60', 'photo_profile':'AAA'"}
 		r = requests.post(self.__api_base_url + self._url_usuario + "signup", json=payload)
 		self.assertEqual(r.status_code, 200)
 		data = json.loads(r.text)
@@ -26,7 +26,7 @@ class AccountTest(unittest.TestCase):
 
 	#Failed signup	username already in use
 	def test_2(self):
-		payload = {'username': "'"+ self._username +"'",'password':'pass'}
+		payload = {"'username': '"+ self._username +"','password':'pass'"}
 		r = requests.post(self.__api_base_url + self._url_usuario + "signup", json=payload)
 		self.assertEqual(r.status_code, 400)
 		data = json.loads(r.text)
@@ -35,23 +35,23 @@ class AccountTest(unittest.TestCase):
 		
 	#Failed signup long username	
 	def test_3(self):
-		payload = {'username': "'"+ self._username_long +"'",'password':'pass'}
+		payload= {"'username': '"+ self._username_long +"', 'password': 'pass', 'name': 'name', 'age': 20, 'gender':'M', 'email':'email@email.com', 'latitude': '-34.58', 'longitude': '-58.60', 'photo_profile':'AAA'"}
 		r = requests.post(self.__api_base_url + self._url_usuario + "signup", json=payload)
 		self.assertEqual(r.status_code, 500)
 		data = json.loads(r.text)
 		self.assertEqual(data["errors"][0]["code"], 6)
 		self.assertEqual(data["errors"][0]["message"], "Server error")
 
-	#Faled signup pass	
+	#Failed signup pass
 	def test_4(self):
-		payload = {'username': "'"+ self._username +"'"}
+		payload = {"'username': '"+ self._username +"'"}
 		r = requests.post(self.__api_base_url + self._url_usuario + "signup", json=payload)
 		self.assertEqual(r.status_code, 400)
 		data = json.loads(r.text)
 		self.assertEqual(data["errors"][0]["code"], 2)
 		self.assertEqual(data["errors"][0]["message"], "Empty password")
 
-	#Faled signup username
+	#Failed signup username
 	def test_5(self):
 		payload = {'password': 'pass'}
 		r = requests.post(self.__api_base_url + self._url_usuario + "signup", json=payload)
@@ -60,7 +60,7 @@ class AccountTest(unittest.TestCase):
 		self.assertEqual(data["errors"][0]["code"], 2)
 		self.assertEqual(data["errors"][0]["message"], "Empty username")
 
-	#Faled signup no user no pass
+	#Failed signup no user no pass
 	def test_6(self):
 		payload = {}
 		r = requests.post(self.__api_base_url + self._url_usuario + "signup", json=payload)
@@ -71,7 +71,7 @@ class AccountTest(unittest.TestCase):
 		self.assertEqual(data["errors"][1]["code"], 2)
 		self.assertEqual(data["errors"][1]["message"], "Empty password")
 
-	#Faled signup bad json
+	#Failed signup bad json
 	def test_7(self):
 		payload = ''
 		r = requests.post(self.__api_base_url + self._url_usuario + "signup", payload)
@@ -82,13 +82,13 @@ class AccountTest(unittest.TestCase):
 		
 	#Successful login
 	def test_8(self):
-		payload = {'username': "'"+ self._username +"'",'password':'pass'}
+		payload = {"'username': '"+ self._username +"','password':'pass'"}
 		r = requests.post(self.__api_base_url + self._url_usuario + "login", json=payload)
 		self.assertEqual(r.status_code, 200)
 
 	#Failed login bad credentials
 	def test_9(self):
-		payload = {'username': "'lalal'",'password':'pass'}
+		payload = {'username': 'username','password':'pass'}
 		r = requests.post(self.__api_base_url + self._url_usuario + "login", json=payload)
 		self.assertEqual(r.status_code, 401)
 		data = json.loads(r.text)
@@ -97,11 +97,12 @@ class AccountTest(unittest.TestCase):
 
 	#Failed login bad password
 	def test_10(self):
-		payload = {'username': "'"+ self._username +"'",'password':'passdifferent'}
+		payload = {"'username': '"+ self._username +"','password':'noValidPassword'"}
 		r = requests.post(self.__api_base_url + self._url_usuario + "login", json=payload)
-		self.assertEqual(r.status_code, 200)
+		self.assertEqual(r.status_code, 401)
 		data = json.loads(r.text)
-		self.assertEqual(data, None)
+		self.assertEqual(data["errors"][0]["code"], 5)
+		self.assertEqual(data["errors"][0]["message"], "Bad credentials")
 
 	#Failed login bad json
 	def test_11(self):
@@ -121,14 +122,14 @@ class AccountTest(unittest.TestCase):
 
 	#Unauthorized like
 	def test_13(self):
-		payload = {'username': "'"+ self._username +"'"}
+		payload = {"'username': '"+ self._username +"'"}
 		r = requests.put(self.__api_base_url + self._url_usuario + "like", json=payload)
 		self.assertEqual(r.status_code, 401)
 		self.assertFalse(r.text)
 
 	#Successful signup other username	
 	def test_14(self):
-		payload = {'username': "'" + self._usernamelike + "'",'password':'pass'}
+		payload= {"'username': '"+ self._usernamelike +"', 'password': 'pass', 'name': 'name', 'age': 20, 'gender':'M', 'email':'email@email.com', 'latitude': '-34.58', 'longitude': '-58.60', 'photo_profile':'AAA'"}
 		r = requests.post(self.__api_base_url + self._url_usuario + "signup", json=payload)
 		self.assertEqual(r.status_code, 200)
 		data = json.loads(r.text)
@@ -144,13 +145,19 @@ class AccountTest(unittest.TestCase):
 
 	#Successful like and dislike 
 	def test_16(self):
-		payload = {'username': "'"+ self._username +"'",'password':'pass'}
+		payload = {"'username': '"+ self._username +"','password':'pass'"}
 		r = requests.post(self.__api_base_url + self._url_usuario + "login", json=payload)
 		data = json.loads(r.text)
 		token = data["data"]["accessToken"]
 
+
+
 		auth = {'Authorization': token}
 		r = requests.put(self.__api_base_url + self._url_usuario +  self._usernamelike + "/like", headers=auth)
+
+		print token
+		print self.__api_base_url + self._url_usuario +  self._usernamelike + "/like"
+
 		self.assertEqual(r.status_code, 200)
 		data = json.loads(r.text)
 		self.assertEqual(data["data"]["message"], "Like successful")
@@ -162,7 +169,7 @@ class AccountTest(unittest.TestCase):
 
 	#Unauthorized dislike
 	def test_17(self):
-		payload = {'username': "'"+ self._username +"'"}
+		payload = {"'username': '"+ self._username +"'"}
 		r = requests.put(self.__api_base_url + self._url_usuario + "dislike", json=payload)
 		self.assertEqual(r.status_code, 401)
 		self.assertFalse(r.text)
